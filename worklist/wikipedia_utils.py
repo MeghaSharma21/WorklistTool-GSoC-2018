a8db5f2f7bd5b0aec27d586a7b6fc7b3cfeee417
Megha Sharma <meghasharma4910@gmail.com>
2018-06-09 22:34:13 +0530

    Resolved bug: Internal failure when no. of titles exceeded 50

--- worklist/wikipedia_utils.py
+++ worklist/wikipedia_utils.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import math
 import mwclient
 
 
@@ -9,11 +10,14 @@ def convert_article_titles_into_ids(titles):
 
     page_title_to_id_mapping = defaultdict(int)
 
-    titles_list = ' | '.join(str(page) for page in titles)
+    for i in range(math.ceil(len(titles) / 50)):
+        # Since at max 50 titles can be passed in one request
+        titles_list = ' | '.join(str(page) for page in
+                                 titles[i * 50:(i + 1) * 50])
 
-    site = mwclient.Site('en.wikipedia.org')
-    result = site.api('query', prop='info', titles=titles_list)
-    for page in result['query']['pages'].values():
-        page_title_to_id_mapping[str(page.get('title'))] = page.get('pageid')
+        site = mwclient.Site('en.wikipedia.org')
+        result = site.api('query', prop='info', titles=titles_list)
+        for page in result['query']['pages'].values():
+            page_title_to_id_mapping[str(page.get('title'))] = page.get('pageid')
 
     return page_title_to_id_mapping
