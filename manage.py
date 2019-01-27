1bbe45d41e1d7dde06a8e05c5f4860ac8489dcf8
MeghaSharma21 <meghasharma4910@gmail.com>
2019-01-27 18:24:09 +0530

    Adding manage.py

---
+++ manage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import sys
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        )
+    execute_from_command_line(sys.argv)
